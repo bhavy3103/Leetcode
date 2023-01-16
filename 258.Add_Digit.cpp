e67ab0b36207daa7da9aaf5a0321492958d95910
Bhavykumar Devani <105499961+bhavy3103@users.noreply.github.com>
2023-01-16 17:13:03 +0530

    Add files via upload

---
+++ 258.Add_Digit.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int addDigits(int num)
+    {
+        int sum = 0;
+        while (num > 0)
+        {
+            int r = num % 10;
+            sum += r;
+            num /= 10;
+            if (num == 0 && sum > 9)
+            {
+                num = sum;
+                sum = 0;
+            }
+        }
+        return sum;
+    }
+};
